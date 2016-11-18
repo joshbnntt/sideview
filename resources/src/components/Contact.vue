bb2f923ac7350410d4c38fd827863dd43bd72478
Joshua Bennett <Joshua.Bennett@cts-america.com>
2016-11-18 16:24:02 -0600

    added element ui - error happening

--- resources/src/components/Contact.vue
+++ resources/src/components/Contact.vue
@@ -3,26 +3,23 @@
       <div v-show='returned'>
           {{returned}}
       </div>
-    <form id="contact-form" v-on:submit.prevent="submit">
-        <div class="field">
-            <input type="text" v-model="company" hidden>
-        </div>
-        <div class="field">
-            <label>Name:</label>
-            <input type="text" v-model="name" required minlength="3">
-        </div>
-        <div class="field">
-            <label>Email:</label>
-            <input type="email" v-model="email" required>
-        </div>
-        <div class="field">
-            <label>Message:</label>
-            <input type="text" v-model="message" required>
-        </div>
-        <div class="field">
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+    <el-form ref="form" :rules="rules" :model="form"  label-width="120px">
+        <el-form-item>
+            <el-input type="text" v-model="form.company" hidden></el-input>
+        </el-form-item>    
+        <el-form-item label="Name" prop="name">
+            <el-input type="text" v-model="form.name" required minlength="3"></el-input>
+        </el-form-item>
+        <el-form-item label="Email" prop="email">
+            <el-input type="email" v-model="form.email" required></el-input>
+        </el-form-item>
+        <el-form-item label="Message" prop="message">
+            <el-input type="textarea" v-model="form.message" required></el-input>
+        </el-form-item>
+        <el-form-item >
+            <el-button type="primary">Submit</el-button>
+        </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -32,38 +29,59 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            name: '',
-            email: '',
-            message: '',
-            token: '',
             company: '',
-            returned: ''
-        };
+            returned: '',
+            form: {
+                name: '',
+                email: '',
+                message: '',
+                token: ''   
+            },
+            rules: {
+                name: [
+                    { required: true, message: 'Please give your name', trigger: 'blur' },
+                    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
+                ],
+                email: [
+                    { required: true, message: 'Please give your email', trigger: 'blur' },
+                ]
+            }
+            
+        }
     },
     mounted() {
         this.token = document.querySelector('#_token').getAttribute('value')
         axios.defaults.headers.common['CSRF-TOKEN'] = this.token
     },
     methods: {
+        handleReset() {
+            this.$refs.Form.resetFields()
+        },
         submit(e) {
-            if(this.company === '') {
-                axios.post('/api/v1/contact', {
-                        name:this.name,
-                        email:this.email,
-                        message:this.message
-                    })
-                    .then((response) => {
-                        this.returned = response.data
-                        this.name = ''
-                        this.email = ''
-                        this.message = ''
-                    })
-                    .catch((error) => {
-                        this.returned = error
-                        console.log(error)
-                    })
+            if(this.form.company === '') {
+                this.$refs.Form.validate((valid) => {
+                    if (valid) {
+                        axios.post('/api/v1/contact', {
+                                name:this.form.name,
+                                email:this.form.email,
+                                message:this.form.message
+                            })
+                            .then((response) => {
+                                this.returned = response.data
+                                this.handleReset()
+                            })
+                            .catch((error) => {
+                                this.returned = error
+                                console.log(error)
+                            })
+                    } else {
+                        this.returned = 'Failed'
+                        return false;
+                    }
+                }
             } else {
                 this.returned = 'Failed'
+                return false;
             }
         }
     }
